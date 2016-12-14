e3b56af15e6b94f342473b5b4849ff08a50c853b
Rich Teague <teague@mpia.de>
2016-12-14 17:14:43 +0100

    update

--- radexcee.py
+++ radexcee.py
@@ -5,49 +5,23 @@ import scipy.constants as sc
 
 class radexcee:
     
-    def __init__(self, paths, snr, grid_path, **kwargs):
-        """
-        spectra   : list of spectra classes from `extractspectra.py`.
-        snr       : list of the snr ratios to use for each line. If
-                    only a single value is given, assume the same for
-                    all the lines.
-        grid_path : path to the pre-calculated line intensity grid.
-        """
-        
-        #TODO: Clean up the whole reading in thing.
-        
-        try:
-            iter(paths)
-        except TypeError:
-            self.paths = [paths]
-        else:
-            self.paths = paths
-        
-        self.spectra = [spectrum(p) for p in self.paths]
-        self.trans = [int(spec.transition) for spec in self.spectra]
-        self.ntrans = len(self.trans)
-        
-        try:
-            iter(snr)
-        except TypeError:
-            self.snr = [snr for s in self.spectra]
-        else:
-            self.snr = snr 
-        
-            
-        if len(self.snr) != len(self.spectra):
-            raise ValueError()
-        toiter = zip(self.spectra, self.snr)
-        self.lines = [spec.noisySpectra(s) 
-                      for spec, s in toiter]
-        self.velax = self.spectra[0].velax
-        self.dvelax = np.diff(self.velax).mean()
-        self.grid = radexgrid(grid_path)
-        self.rms = [np.nanmax(spec.noisySpectra(s)) / s for spec, s in toiter]
-        
-        
+    def __init__(self, obsdict, grid_path):
+        """Initialise."""
+        self.parse_to_dictionaries(obsdict)     
+        self.grid = radexgrid(grid_path)       
         return
-        
+    
+    def parse_to_dictionaries(self, obsdict):
+        """Splits the provided dictionary into several dictionaries."""
+        self.trans = sorted(obsdict.keys())
+        self.ntrans = len(self.trans)
+        self.velaxes = {j : obsdict[j]['velax'] for j in self.trans}
+        self.dvelaxes = {j : np.diff(obsdict[j]['velax']).mean() for j in self.trans}
+        self.lines = {j : obsdict[j]['intensity'] for j in self.trans}
+        self.rms = {j : obsdict[j]['rms'] for j in self.trans}
+        self.fluxcal = {j : obsdict[j]['fluxcal'] for j in self.trans}
+        return
+    
     def lnpriors(self, theta, gradients):
         """Log-prior function."""
         
@@ -79,7 +53,7 @@ class radexcee:
         return 0.0
 
 
-    def lnprob(self, theta, fluxcal=0.0, gradients=[0, 0, 0], nslabs=5):
+    def lnprob(self, theta, gradients=[0, 0, 0], nslabs=5):
         """Log-probability function."""  
         
         # Number of slabs.
@@ -89,36 +63,27 @@ class radexcee:
         lp = self.lnpriors(theta, gradients)
         if not np.isfinite(lp):
             return -np.inf
-
-        # Flux calibration for each of the lines. This will be inclued in 
-        # the uncertainties and as a rescaling of the model spectra.
-        fluxcal = self.check_iterable(fluxcal) 
         
         # Likelihood.
-        return lp + self.lnlike(theta, fluxcal, gradients)
+        return lp + self.lnlike(theta, gradients)
 
         
-    def lnlike(self, theta, fluxcal, gradients):
-        """
-        Log-likelihood function.
-        """
+    def lnlike(self, theta, gradients):
+        """Log-likelihood function."""
         
         # Each model is the summation of homogeneous slab models.               
-        models = [self.calc_models(j, theta, fc, gradients) 
-                  for j, fc in zip(self.trans, fluxcal)]
+        models = [self.calc_models(j, theta, gradients) for j in self.trans]
 
         # Use Chi-square as the distance metric.
-        toiter = zip(models, self.lines, self.rms, fluxcal)
-        lnlike = [self.lnchi2(m, o, u, c) for m, o, u, c in toiter]
-        return np.nansum(lnlike)
+        ll = [self.lnchi2(model, self.lines[j], self.rms[j], self.fluxcal[j]) 
+              for model, j in zip(models, self.trans)]
+        return np.nansum(ll)
 
         
-    def lnchi2(self, model, obs, rms, fluxcal=0.0):
-        """
-        Returns the log-chi_squared value. Also models the flux calibration.
-        """
+    def lnchi2(self, model, observation, rms, fluxcal):
+        """Returns the log-chi_squared value."""
         unc = np.hypot(rms, model * fluxcal)        
-        lnx2 = ((obs - model) / unc)**2
+        lnx2 = ((observation - model) / unc)**2
         lnx2 -= 0.5 * np.log(2. * np.pi * unc**2)
         return -0.5 * np.sum(lnx2)
 
@@ -131,7 +96,7 @@ class radexcee:
         return [theta[4+sum(g[:i])] if g[i] else 0. for i in range(len(g))]
 
 
-    def calc_models(self, j, theta, fluxcal, gradients):
+    def calc_models(self, j, theta, gradients):
         """Build a spectra from several slab models."""
         
         # Calculate the parameters for each slab model.
@@ -141,13 +106,12 @@ class radexcee:
                      self.param_gradient(theta[2], ranges[2]),
                      self.slab_columns(theta[3]))       
         
-        # Generate a spectrum for each slab then combine.
-        # Just simply add them, need to update this for thicker lines.
-        mods = np.array([self.grid.get_spectra(j, p, self.velax) for p in params])
+        # Combine slabs.
+        mods = [self.grid.get_spectra(j, p, self.velaxes[j]) for p in params]
         mods = np.sum(mods, axis=0)
         
         # Include flux calibration.
-        scale = 1. + np.random.randn() * fluxcal
+        scale = 1. + np.random.randn() * self.fluxcal[j]
         return abs(scale) * mods
 
 
@@ -176,22 +140,4 @@ class radexcee:
             else:
                 raise ValueError('Mismatch in lengths of iterables.')
         return [inp for i in range(self.ntrans)]
-        
-    '''
-    ## Obsolete. ##
-    def temperature_step(self, T):
-        """Returns the appropriate temperature step."""
-        # Note the 1e3 factor is to account for the [km/s] > [m/s] change.
-        sigma_fwhm = self.sigma * self.dvelax * 1e3 / self.snr[0] / 0.6
-        mean_width = self.thermal_width(T)
-        delta_temp = (mean_width - sigma_fwhm)**2
-        delta_temp *= self.mu * sc.m_p / 2. / sc.k
-        delta_temp = abs(delta_temp - T)
-        return delta_temp 
-        
-    ## Obsolete. ##
-    def thermal_width(self, T):
-        """Thermal width of a line."""
-        return np.sqrt(2. * sc.k * T / self.mu / sc.m_p)
-   '''
-        
+ 
