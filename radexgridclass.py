ed3d042d1f608fc7745a80188c3402817d4c773d
Rich Teague <teague@mpia.de>
2017-01-24 17:11:57 +0100

    included radexceeII

--- radexgridclass.py
+++ radexgridclass.py
@@ -19,9 +19,9 @@ class radexgrid:
         self.mu = self.read_molecular_weight()
         self.parse_filename()
         self.fwhm = 2. * np.sqrt(np.log(2) * 2)
-        
+
         self.gridranges = [self.vals[i*3+1] - self.vals[i*3] for i in range(4)]
-        
+
         return
 
     def parse_filename(self):
@@ -40,63 +40,63 @@ class radexgrid:
         return
 
     # Interpolation routines. Uses linear interpolation but higher orders can
-    # be specified through 'order=2'. 
+    # be specified through 'order=2'.
     # j : index of the transition.
-    # t : [non-thermal width [m/s], 
-    #      kinetic temperature [K], 
-    #      log10(nH2 number density [cm^-3]), 
+    # t : [non-thermal width [m/s],
+    #      kinetic temperature [K],
+    #      log10(nH2 number density [cm^-3]),
     #      log10(column density [cm^-2])]
-    
-    def interpolate_indices(self, j, vals):
+
+    def interpolate_indices(self, j, width, temp, rho, sigma):
         """Returns the values required for the interpolation."""
-        width = self.total_linewidth(vals) * self.fwhm
         w = np.interp(width, self.linewidth, np.arange(self.vals[2]))
-        t = np.interp(vals[1], self.temperature, np.arange(self.vals[5]))
-        r = np.interp(vals[2], self.logdensity, np.arange(self.vals[8]))
-        s = np.interp(vals[3], self.logsigma, np.arange(self.vals[11]))
+        t = np.interp(temp, self.temperature, np.arange(self.vals[5]))
+        r = np.interp(rho, self.logdensity, np.arange(self.vals[8]))
+        s = np.interp(sigma, self.logsigma, np.arange(self.vals[11]))
         return [[w], [t], [r], [s]]
 
-    def interpolate_intensity(self, j, vals):
+    def interpolate_intensity(self, j, width, temp, rho, sigma):
         """Interpolate the intensity grid."""
-        indices = self.interpolate_indices(j, vals)
+        indices = self.interpolate_indices(j, width, temp, rho, sigma)
         return map_coordinates(self.grid[j,0], indices, order=1, mode='nearest')
-    
-    def interpolate_tau(self, j, vals):
+
+    def interpolate_tau(self, j, width, temp, rho, sigma):
         """Interpolate the optical depth grid."""
-        indices = self.interpolate_indices(j, vals)
+        indices = self.interpolate_indices(j, width, temp, rho, sigma)
         return map_coordinates(self.grid[j,1], indices, order=1, mode='nearest')
 
     # Model spectra.
-    # For a given velocity axis, 'velax' [km/s], generate a spectrum from 
-    # a slab specified by 't'. If attenuate is True then the returned 
+    # For a given velocity axis, 'velax' [km/s], generate a spectrum from
+    # a slab specified by 't'. If attenuate is True then the returned
     # spectrum is attenuated by a factor of (1 - exp(-tau)).
- 
+
     def gaussian(self, x, a, b, c):
         """Simple Gaussian function, width is Doppler-b."""
         return a * np.exp(-0.5 * np.power((x - b) / c, 2))
 
-    def get_spectra(self, j, vals, velax):
+    def get_spectra(self, j, temp, sigma, rho, centre, width, velax):
         """Single Gaussian spectrum."""
-        return self.gaussian(velax, self.interpolate_intensity(j, vals), 0.0, vals[0])
-            
+        Tb = self.interpolate_intensity(j, width * self.fwhm, temp, rho, sigma)
+        return self.gaussian(velax, Tb, centre, width)
+
     def get_tau(self, j, t, velax):
         """Returns the (Gaussian) optical depth profile for the slab. """
         peak_opacity = self.interpolate_tau(j, t)
         line_center = 0.0
         linewidth = self.total_linewidth(t)
         return self.gaussian(velax, peak_opacity, line_center, linewidth)
-    
+
     def total_linewidth(self, vals, kms=True):
         """Returns total Doppler-b linewidth."""
         linewidth = np.hypot(vals[0], self.thermal_linewidth(vals))
         if kms:
             linewidth /= 1e3
         return linewidth
-    
+
     def thermal_linewidth(self, vals):
         """Returns the thermal Doppler-b linewidth."""
         return np.sqrt(2. * sc.k * abs(vals[1]) / self.mu / sc.m_p)
-        
+
     # Additional functions.
 
     def randomValues(self, turb_only=True, fwhm=False, kms=False):
@@ -117,15 +117,14 @@ class radexgrid:
         if not kms:
             vals[0] *= 1e3
         return vals
-     
+
     def randomValue(self, i):
         """Returns a random value for the ith variable."""
         return np.random.ranf() * (self.vals[1+(i*3)] - self.vals[(i*3)]) + self.vals[i*3]
-    
+
     def read_molecular_weight(self):
         """Read the molecular weight from collisional rates."""
         mol = self.filename.split('_')[0]
         path = os.getenv('RADEX_DATAPATH')
         rates = ratefile(path+mol+'.dat')
         return rates.mu
-
